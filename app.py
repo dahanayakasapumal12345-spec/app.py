951918cb4e1ff3cabac7b46f6a75584e6645e6a4
dahanayakasapumal12345-spec <dahanayakasapumal12345@gmail.com>
2026-08-20 17:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def fetch_latest_posts():
     
     # 1. Fetch Latest Facebook Post
     try:
-        fb_url = f"https://graph.facebook.com/v19.0/{FB_PAGE_ID}/published_posts?fields=message,attachments{{media_type,media,subattachments}}&limit=1&access_token={ACCESS_TOKEN}"
+        fb_url = f"https://graph.facebook.com/v19.0/{FB_PAGE_ID}/published_posts?fields=message,attachments{{media_type,media,subattachments}}&limit=5&access_token={ACCESS_TOKEN}"
         fb_res = requests.get(fb_url).json()
         if 'data' in fb_res and len(fb_res['data']) > 0:
             post_data = fb_res['data'][0]
@@ -60,7 +60,7 @@ def fetch_latest_posts():
 
     # 2. Fetch Latest Instagram Post
     try:
-        ig_url = f"https://graph.facebook.com/v19.0/{INSTAGRAM_ID}/media?fields=caption,media_type,media_url&limit=1&access_token={ACCESS_TOKEN}"
+        ig_url = f"https://graph.facebook.com/v19.0/{INSTAGRAM_ID}/media?fields=caption,media_type,media_url&limit=5&access_token={ACCESS_TOKEN}"
         ig_res = requests.get(ig_url).json()
         if 'data' in ig_res and len(ig_res['data']) > 0:
             post_data = ig_res['data'][0]
